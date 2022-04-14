78c55ba42f7a02450abed69c333d6785c57b1ab4
k.panchenko <k.panchenko@simcord.com>
2022-04-14 21:13:04 +0300

    Text validation

--- src/main/java/com/example/serverdemo/TokenizerController.java
+++ src/main/java/com/example/serverdemo/TokenizerController.java
@@ -1,11 +1,14 @@
 package com.example.serverdemo;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -19,5 +22,14 @@ public class TokenizerController {
         ));
         map.remove("");
         return map;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getAllErrors().stream().findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("Unknown error");
     }
 }
