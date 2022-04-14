545d4105e82a5143a2fb4454a3c28b55bc8399d4
k.panchenko <k.panchenko@simcord.com>
2022-04-14 21:02:20 +0300

    Text validation

--- src/main/java/com/example/serverdemo/Request.java
+++ src/main/java/com/example/serverdemo/Request.java
@@ -1,8 +1,18 @@
 package com.example.serverdemo;
 
 
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
+
+import javax.validation.constraints.NotBlank;
+
 public class Request {
+    @NotBlank(message = "Text is null")
     private String text;
+
+    public Request() {
+        text = null;
+    }
 
     public String getText() {
         return text;
--- src/test/java/com/example/serverdemo/ServerDemoApplicationTests.java
+++ src/test/java/com/example/serverdemo/ServerDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.serverdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerDemoApplicationTests {
+    @Autowired
+    TokenizerController tokenizerController;
 
     @Test
-    void contextLoads() {
-    }
+    void name() {
+        var request = new Request();
 
+        var request2 = request.setText("");
+
+        assert request == request2;
+    }
 }
