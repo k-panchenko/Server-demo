d28cd016c7fc52471a865d48e156cdbdcfd42dc4
k.panchenko <k.panchenko@simcord.com>
2022-04-14 21:03:34 +0300

    Text validation

--- src/main/java/com/example/serverdemo/TokenizerController.java
+++ src/main/java/com/example/serverdemo/TokenizerController.java
@@ -1,5 +1,6 @@
 package com.example.serverdemo;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
 @RestController
 public class TokenizerController {
     @PostMapping("tokenize")
-    public Map<String, Integer> tokenize(@RequestBody Request str) {
+    public Map<String, Integer> tokenize(@Validated @RequestBody Request str) {
         var map = Arrays.stream(str.getText().split("\\s+")).collect(Collectors.groupingBy(
                 s -> s.replaceAll("[^\\w]", "").toLowerCase(),
                 Collectors.summingInt(__ -> 1)
